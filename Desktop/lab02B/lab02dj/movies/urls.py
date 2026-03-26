bce0436157d2c70282d154422df8bba42b1ca6b8
Naomi <solanch.veliz@tecsup.edu.pe>
2026-03-26 10:34:58 -0500

    Carpeta Naomi

--- Desktop/lab02B/lab02dj/movies/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import lista_movies
-
-urlpatterns = [
-    path('', lista_movies),
-]
--- Desktop/lab02B/lab02dj/movies/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Movie
-
-def lista_movies(request):
-    movies = Movie.objects.all()
-    return render(request, 'movies/lista.html', {'movies': movies})
